5bbd0f83ef00ef57ec103fabe34c26a9f2003659
BuildFailedYT <145024358+BuildFailedYT@users.noreply.github.com>
2023-10-06 07:01:31 -0500

    Update ts

--- src/example.ts
+++ src/example.ts
@@ -1,9 +1,10 @@
-enum Languages {
+enum Language {
   Javascript = "javascript",
   Typescript = "typescript",
 }
 
 console.log("Hi Youtube!");
+console.log("We are debugging:", Language.Typescript);
 
 for (let i = 0; i < 10; i++) {
   console.log("I am looping, index", i);
