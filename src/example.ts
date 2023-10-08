8a16c13904ff3e181f69dd44c5721681be365808
BuildFailedYT <145024358+BuildFailedYT@users.noreply.github.com>
2023-10-08 16:19:07 -0500

    Logic

--- src/example.ts
+++ src/example.ts
@@ -5,8 +5,8 @@ enum Language {
 
 function printAyo(type: Language) {
   let ayo = 0;
-  for (let i = 0; i < 10; i++) {
-    ayo = i * 10;
+  for (let i = 0; i < 3; i++) {
+    ayo = i * (type === Language.Javascript ? 0 : 10);
   }
 }
 
